3391c4b21bfc7e48bb5dcfe906c6adae0392bced
jhover <jhover@transit>
2019-03-27 13:13:44 -0400

    Update.

--- parsl/hello_htcondor.py
+++ parsl/hello_htcondor.py
@@ -18,7 +18,7 @@ config = Config(
                 nodes_per_block=1,
                 init_blocks=1,
                 max_blocks=1,
-                scheduler_options='ProjectName="OSG.sPHENIX"',     # Input your scheduler_options if needed
+                scheduler_options='+ProjectName="OSG.sPHENIX"',     # Input your scheduler_options if needed
                 worker_init='',     # Input your worker_init if needed
                 requirements='',
                 
